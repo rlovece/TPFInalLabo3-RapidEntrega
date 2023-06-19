6b30289d96686bef749fcad34c9d11b84113f0f2
rlovece <ruth.lovece@gmail.com>
2023-06-19 16:31:33 -0300

    Se agrega menu en main, y logueo Admin más menuPrincipalAdmin

--- src/main/java/org/example/gestiones/GestionAdmin.java
+++ src/main/java/org/example/gestiones/GestionAdmin.java
@@ -2,10 +2,13 @@ package org.example.gestiones;
 
 import org.example.enums.EstadosPaquete;
 import org.example.excepciones.CodigoPaqueteExistente;
+import org.example.excepciones.PasswordInvalida;
 import org.example.interfacesDeManejo.ManejoPaquete;
+import org.example.models.Cliente;
 import org.example.models.Paquete;
+import org.example.models.Supervisor;
 import org.example.recursos.EntradaSalida;
-import org.example.repositorio.PaqueteRepo;
+import org.example.repositorio.*;
 
 import javax.swing.*;
 import java.time.LocalDateTime;
@@ -13,6 +16,65 @@ import java.time.LocalDateTime;
 public class GestionAdmin implements ManejoPaquete {
 
     PaqueteRepo paqueteRepo = new PaqueteRepo();
+    SupervisorRepo supervisorRepo = new SupervisorRepo();
+    RepartidorRepo repartidorRepo = new RepartidorRepo();
+    EmpleadoLocalRepo empleadoLocalRepo = new EmpleadoLocalRepo();
+    ClientesRepo clientesRepo = new ClientesRepo();
+
+    //region Logueo
+    public boolean logueo(){
+        try {
+            String password = EntradaSalida.entradaString("Ingrese la contraseña");
+            validarPassword(password);
+            menuPrincipal();
+            return true;
+        } catch (NullPointerException e) {
+            return false;
+        } catch (PasswordInvalida e) {
+            EntradaSalida.SalidaError("\nContraseña incorrecta\n", "Error");
+            return false;
+        }
+    }
+    //endregion
+
+    public void menuPrincipal(){
+        int opcion = 0;
+        do {
+            opcion = EntradaSalida.entradaInt("      ELIJA OPCION  \n" +
+                    "\n 1 - Manejar Paquetes" +
+                    "\n 2 - Manejar Empleados" +
+                    "\n 0 - Cerrar Sesión\n\n");
+
+            switch (opcion){
+                case 1:
+                    menuManejoPaquetes();
+                    break;
+
+                case 2:
+                    menuManejoEmpleados();
+                    break;
+
+                default:
+                    break;
+            }
+        } while (opcion!=0);
+    }
+
+
+    //region Manejo Paquetes
+    public void menuManejoPaquetes(){
+        //FALTA IMPLEMENTAR
+    };
+
+    //endregion
+
+
+    //region Manejo Empleados
+    public void menuManejoEmpleados(){
+        //FALTA IMPLEMENTAR
+    };
+    //endregion
+
 
     @Override
     public boolean modificarPaquete(int id) {
@@ -69,6 +131,12 @@ public class GestionAdmin implements ManejoPaquete {
                 throw new CodigoPaqueteExistente("Codigo Paquete Existente");
             }
             break;
+        }
+    }
+
+    static void validarPassword (String contraseña) throws PasswordInvalida {
+        if (!contraseña.equals("123Admin")){
+            throw new PasswordInvalida("Contraseña incorrecta!");
         }
     }
 }
